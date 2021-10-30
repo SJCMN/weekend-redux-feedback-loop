1c397189f60c706d7f58b6dd9ccf376336a255f4
SJCMN <sam.clavette@gmail.com>
2021-10-30 11:53:28 -0500

    router POST working, test with PostMan

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -13,4 +13,22 @@ router.get('/', (req,res) => {
     })
 })
 
+router.post('/', (req,res) => {
+    console.log('Feedback Route POST');
+    let newFeedback = req.body
+    const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") 
+                        VALUES ($1, $2, $3, $4);`
+
+
+
+    pool.query( queryText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments] )
+    .then((result) => {
+    res.sendStatus(201);
+    }).catch((error) => {
+        console.log('Error POST /feedback', error);
+        res.sendStatus(500)        
+    })
+})
+
+
 module.exports = router;
